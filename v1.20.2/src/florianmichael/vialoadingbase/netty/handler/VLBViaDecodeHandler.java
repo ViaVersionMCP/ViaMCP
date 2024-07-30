a919caff766e1108a9eaa1e7647d24d85f32214d
NickRest <nickrestemail@gmail.com>
2024-07-30 08:28:29 -0400

    change folder name

---
+++ v1.20.2/src/florianmichael/vialoadingbase/netty/handler/VLBViaDecodeHandler.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of ViaLoadingBase - https://github.com/FlorianMichael/ViaLoadingBase
+ * Copyright (C) 2020-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.florianmichael.vialoadingbase.netty.handler;
+
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.connection.UserConnection;
+import com.viaversion.viaversion.api.protocol.packet.State;
+import com.viaversion.viaversion.exception.CancelCodecException;
+import com.viaversion.viaversion.exception.CancelDecoderException;
+import com.viaversion.viaversion.exception.InformativeException;
+import com.viaversion.viaversion.util.PipelineUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.util.List;
+
+@ChannelHandler.Sharable
+public class VLBViaDecodeHandler extends MessageToMessageDecoder<ByteBuf> {
+    private final UserConnection user;
+
+    public VLBViaDecodeHandler(UserConnection user) {
+        this.user = user;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf bytebuf, List<Object> out) throws Exception {
+        if (!user.checkIncomingPacket()) throw CancelDecoderException.generate(null);
+        if (!user.shouldTransformPacket()) {
+            out.add(bytebuf.retain());
+            return;
+        }
+
+        ByteBuf transformedBuf = ctx.alloc().buffer().writeBytes(bytebuf);
+        try {
+            user.transformIncoming(transformedBuf, CancelDecoderException::generate);
+
+            out.add(transformedBuf.retain());
+        } finally {
+            transformedBuf.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        if (PipelineUtil.containsCause(cause, CancelCodecException.class)) return;
+
+        if ((PipelineUtil.containsCause(cause, InformativeException.class)
+                && user.getProtocolInfo().getServerState() != State.HANDSHAKE)
+                || Via.getManager().debugHandler().enabled()) {
+            cause.printStackTrace();
+        }
+    }
+}
